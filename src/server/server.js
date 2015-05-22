5041b51af0dd1454f006e0407e1c417d4eb7120a
Janic <janicduplessis@gmail.com>
2015-05-22 01:12:30 -0400

    work on server

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,11 @@
-import express from 'express';
+import express from 'express.io';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import nconf from 'nconf';
 import db from 'rethinkdb';
 
 import UserStore from './UserStore';
+import MessageStore from './MessageStore';
 
 nconf.argv()
   .env()
@@ -16,17 +17,28 @@ const config = {
     port: nconf.get('db:port'),
     authKey: nconf.get('db:authKey'),
   },
+  server: {
+    port: nconf.get('server:port'),
+  },
 };
 
+console.info('Starting iMessage server...\n');
+console.info('Config:');
+console.info('db:', nconf.get('db'));
+console.info('server:', nconf.get('server'), '\n');
+
 const app = express();
+app.http().io();
+
 let userStore = null;
+let messageStore = null;
 
 app.use(morgan('dev'));
 app.use('/', express.static('static'));
 app.use(bodyParser.json());
 
 /**
- * Index
+ * Index handler.
  */
 app.get('/', (req, res) => {
   const webserver = process.env.NODE_ENV === 'production' ? '' : '//localhost:8080';
@@ -47,6 +59,9 @@ app.get('/', (req, res) => {
   res.send(output);
 });
 
+/**
+ * Login handler.
+ */
 app.post('/api/login', async (req, res) => {
   let { username, password } = req.body;
 
@@ -66,6 +81,9 @@ app.post('/api/login', async (req, res) => {
   });
 });
 
+/**
+ * Register handler.
+ */
 app.post('/api/register', (req, res) => {
   let {
     username,
@@ -91,23 +109,95 @@ app.post('/api/register', (req, res) => {
   });
 });
 
-function startServer() {
-  let server = app.listen(process.env.PORT || 8000, () => {
-    let host = server.address().address;
-    let port = server.address().port;
-
-    console.info(`==> ✅  Server is listening\n==> 🌎  Go to http://${host}:${port}`);
-  });
-}
-
-db.connect({
-  host: config.database.host,
-  port: config.database.port,
-  db: 'imessagechat',
-  /*authKey: config.database.authKey,*/
-}).then((conn) => {
-  userStore = new UserStore(conn);
-  startServer();
-}).catch((err) => {
-  console.error(err);
+app.io.route('send', (req) => {
+  if(req.type === 'client') {
+    // If we receive a new message from the web client we send it
+    // to the mac client.
+    req.io.room(req.user.id + '-mac').broadcast('message', req.message);
+  } else {
+    // If we receive a new message from the mac client we save it to
+    // the database. When the database receives new messages it will
+    // notify the web clients via a change handler.
+    messageStore.add(req.user.id, req.message);
+  }
 });
+
+app.io.route('ready', (req) => {
+  if(req.type === 'client') {
+    // Add a web client to the user group.
+    req.io.join(req.user.id + '-client');
+
+    // When the message store receives a new message notify the web clients for
+    // that user.
+    messageStore.addMessageListener(req.user.id, (message) => {
+      console.log('New message for user:', req.user.id, message);
+      req.io.room(req.user.id + '-client').broadcast('message', message);
+    });
+  } else {
+    // Add a mac client to the user group.
+    req.io.join(req.user.id + '-mac');
+  }
+});
+
+(async () => {
+  // Init database.
+  console.info(`Connecting to database at ${config.database.host}:${config.database.port}...`);
+  let conn;
+  try {
+    conn = await db.connect({
+      host: config.database.host,
+      port: config.database.port,
+      db: 'imessagechat',
+      /*authKey: config.database.authKey,*/
+    });
+  } catch(error) {
+    console.error(`Error connecting to database.\n${error}`);
+    return;
+  }
+  console.info('Connected to database');
+
+  // Create the database and tables if necessary.
+  console.info('Creating database and tables...');
+  try {
+    await db.dbCreate('imessagechat').run(conn);
+    console.info('Created database imessagechat.');
+  } catch(error) {
+    console.info('Database imessagechat already exists.');
+  }
+  try {
+    await db.tableCreate('users').run(conn);
+    console.info('Created table users.');
+  } catch(error) {
+    console.info('Table users already exists.');
+  }
+  try {
+    await db.tableCreate('messages').run(conn);
+    console.info('Created table messages.');
+  } catch(error) {
+    console.info('Table messages already exists.');
+  }
+
+  // Create stores.
+  userStore = new UserStore(conn);
+  messageStore = new MessageStore(conn);
+
+  try {
+    await messageStore.init();
+  } catch(err) {
+    console.log('Error initializing the message store.', err);
+    return;
+  }
+
+  messageStore.addMessageListener('90d0fd03-a617-4ee8-8385-10174d6d0c87', (message) => {
+    console.log(message);
+  });
+
+  let messages = await messageStore.list('90d0fd03-a617-4ee8-8385-10174d6d0c87');
+  console.log(messages);
+
+  // Start the server.
+  console.info(`Starting server on port ${config.server.port}...`);
+  app.listen(config.server.port, () => {
+    console.info('Server started.');
+  });
+})();
